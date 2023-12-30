8d5f0c20ff5bb00a389914bd4fdb5f21e841a655
jitae-moon <jitae.moon@icloud.com>
2023-12-30 23:15:37 +0900

    Time: 5 ms, Memory: 44.9 MB - LeetHub

---
+++ linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        Set<ListNode> visited = new HashSet<>();
+        
+        while (head != null) {
+            if (visited.contains(head)) return head;
+            else visited.add(head);
+            
+            head = head.next;
+        }
+        
+        return null;
+    }
+}
